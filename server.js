ed9708a5b105bf42142d134e1b6b66833a490ceb
David Chen <zdent09@gmail.com>
2020-10-18 15:13:12 +0800

    npm i bcrypt

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello')
+})
+
+app.post('/login', (req, res) => {
+  // authenticate user
+
+  // create and send jsonwebtoken
+})
+
+app.post('/logout', (req, res) => {
+  // logout
 })
 
 app.listen(3000)
