7018743ea699474df4d609d658057d1bbf190963
David Chen <zdent09@gmail.com>
2020-10-22 13:37:38 +0800

    salt and hash password for signup api

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
+const users = []
+
 app.get('/', (req, res) => {
   res.send('Hello')
+})
+
+app.post('/signup', async (req, res) => {  
+  const user = { username: req.body.username, password: req.body.password }
+
+  if (!user.username || !user.password) {
+    res.status(422).send('bad request')
+  }
+  
+  try {
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(user.password, salt)
+    user.password = hashedPassword
+
+    // create user
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  } 
 })
 
 app.post('/login', (req, res) => {
@@ -19,4 +42,4 @@ app.post('/logout', (req, res) => {
   // logout
 })
 
-app.listen(3000)
+app.listen(3333)
