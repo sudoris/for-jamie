30626ee52b0574f56297bebcfe0a0ab999d16b2a
David Chen <zdent09@gmail.com>
2020-10-22 13:57:05 +0800

    setup login api

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post('/signup', async (req, res) => {
   const user = { username: req.body.username, password: req.body.password }
 
   if (!user.username || !user.password) {
-    res.status(422).send('bad request')
+    res.status(422).send()
   }
   
   try {
@@ -32,8 +32,22 @@ app.post('/signup', async (req, res) => {
   } 
 })
 
-app.post('/login', (req, res) => {
-  // authenticate user
+app.post('/login', async (req, res) => {
+  // check that user exists
+  const user = users.find(user => user.username === req.body.username)
+  if (!user) {
+    return res.status(400).send('login failed')
+  }
+
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Sucess')
+    } else {
+      res.send('login failed')
+    }
+  } catch {
+    res.status(500).send()
+  }
 
   // create and send jsonwebtoken
 })
