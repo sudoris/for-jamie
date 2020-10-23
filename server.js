892bf7b2621ab1622683e1b49437c5ba7e4a3cc9
David Chen <zdent09@gmail.com>
2020-10-23 08:59:14 +0800

    fix jwt token expire time on creation (incorrectly thought default time unit was ms)

--- server.js
+++ server.js
@@ -11,15 +11,24 @@ app.use(express.json())
 const users = []
 
 app.get('/', (req, res) => {
-  return res.send('Hello')
+  return res.send(users)
 })
 
 app.post('/signup', async (req, res) => {  
-  const user = { username: req.body.username, password: req.body.password }
-
-  if (!user.username || !user.password) {
+  if (!req.body.username || !req.body.password) {
     return res.sendStatus(422)
   }
+
+  const user = { 
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    gender: req.body.gender,
+    birthday: req.body.birthday,
+    school: req.body.school,
+    grade: req.body.grade    
+  }  
   
   try {
     const salt = await bcrypt.genSalt()
@@ -45,18 +54,20 @@ app.post('/login', async (req, res) => {
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
       // create and send jsonwebtoken
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
       return res.send({ accessToken })
     } else {
       return res.sendStatus(403)
     }
   } catch {
+
+    
     return res.sendStatus(500)
   }  
 })
 
-app.post('/logout', (req, res) => {
-  // logout
+app.delete('/logout', (req, res) => {
+  
 })
 
 
@@ -74,7 +85,11 @@ function authenticateToken(req, res, next) {
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
+    if (err) {
+      console.log(new Date().toISOString())
+      console.log(err)
+      return res.sendStatus(403)
+    }
     req.user = user
     next()
   })
